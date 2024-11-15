0b8560c8ccd64df8de4142720fe08d84d743075b
JulissaGro <2230163@upv.edu.mx>
2024-11-15 08:51:35 -0600

    landing definido, carpetas para resto de vistas

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::view('/rols/user','rols.rols_view')->name('rolsUser.index');
 Route::view('/users','users.users_view')->name('users.index');
 
 /*Danna*/
-/* Route::view('/landing','layouts.landing')->name('landing');
-Route::view('/landing2','layouts.landing2')->name('landing2'); */
+Route::view('/landing','layouts.landing')->name('landing');
+Route::view('/landing2','layouts.landing2')->name('landing2');
 Route::view('/login','users.login')->name('login');
 Route::view('/register','users.register')->name('register');
