75458f1834ed975b8fff16c04b6d78131ff18a97
Jesus Nuñez <2230224@upv.edu.mx>
2024-11-17 22:43:21 +0000

    La gran corrida

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,16 +16,22 @@ class UsersController extends Controller
         return view('chuchoLab.createUser');
     }
 
-    public function store(Request $request){
+    public function store(Request $request) {
         User::create($request->all());
-        /* User::create([
-            'name' => $request->name,
-            'lastname' => $request->lastname,
-            'date_birth' => $request->date,
-            'phone' => $request->phone,
-            'email' => $request->email,
-            'password' => $request->password,
-        ]); */
+        return redirect()->route('user.show');
+    }
+
+    public function edit(User $user){
+        return view('chuchoLab.editUser',compact('user'));
+    }
+
+    public function update(Request $request, User $user){
+        $user->update($request->all());
+        return redirect()->route('user.show');
+    }
+
+    public function delete(User $user){
+        $user->delete();
         return redirect()->route('user.show');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users/',[UsersController::class,'show'])->name('user.show');
 Route::get('/createUser/',[UsersController::class, 'create'])->name('user.create');
 Route::post('/users/store/',[UsersController::class, 'store'])->name('user.store');
+Route::get('/users/edit/{user}',[UsersController::class, 'edit'])->name('user.edit');
+Route::put('/user/update/{user}', [UsersController::class,'update'])->name('user.update');
+Route::delete('/user/delete/{user}', [UsersController::class, 'delete'])->name('user.delete');
 
 //Borrar esta linea
 Route::view('/', 'welcome');
