3ae741c51e4a09977fb999544fca306c1242516d
Jesus Nuñez <2230224@upv.edu.mx>
2024-11-27 13:37:48 +0000

    Ya tenemos paises y cosas de modelos

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Sponsor extends Model
 {
+
+    public $timestamps = false; 
+
     use HasFactory;
     protected $fillable = [
         'name',
@@ -15,4 +19,9 @@ class Sponsor extends Model
         'location',
         'type',
     ];
+
+    public function users():BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'sponsor_user')->withPivot('mount', 'date_hour', 'concept', 'type');
+    }
 }
