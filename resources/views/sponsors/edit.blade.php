5f2e052e39ea3c19fc9a979807bf6c6d8592e144
JulissaGro <2230163@upv.edu.mx>
2024-11-29 07:53:41 -0600

    push antes de merge con Danna

--- resources/views/sponsors/edit.blade.php
+++ resources/views/sponsors/edit.blade.php
@@ -1,52 +1,51 @@
 @extends('layouts.admin_view')
 
-@section('title', 'Registrar Patrocinador')
+@section('title', 'Editar Patrocinador')
 
 @section('content')
     <h1 class="text-4xl text-center">
-        Registrar Patrocinador
+        Editar Patrocinador
     </h1>
     <div class="flex items-center justify-center">
         <form method="POST" action="#"
             class="flex flex-col bg-stone-900 text-white p-6 rounded-lg shadow-lg w-full max-w-md space-y-4 mt-6">
             @csrf
 
-            @component('_components.boxInputCreate')
+            @component('_components.boxInputEdit')
                 @slot('for', 'name')
                 @slot('content', 'Nombre: ')
                 @slot('type', 'text')
-                @slot('name', 'name')
+                @slot('value','valor')
                 @slot('id', 'name')
             @endcomponent
 
-            @component('_components.boxInputCreate')
+            @component('_components.boxInputEdit')
                 @slot('for', 'phone')
                 @slot('content', 'Teléfono: ')
                 @slot('type', 'phone')
-                @slot('name', 'phone')
+                @slot('value','valor')
                 @slot('id', 'phone')
             @endcomponent
 
-            @component('_components.boxInputCreate')
+            @component('_components.boxInputEdit')
                 @slot('for', 'email')
                 @slot('content', 'Correo: ')
                 @slot('type', 'email')
-                @slot('name', 'email')
+                @slot('value','valor')
                 @slot('id', 'email')
             @endcomponent
 
-            @component('_components.boxInputCreate')
+            @component('_components.boxInputEdit')
                 @slot('for', 'location')
                 @slot('content', 'Ubicación: ')
                 @slot('type', 'text')
-                @slot('name', 'location')
+                @slot('value','valor')
                 @slot('id', 'location')
             @endcomponent
 
             @component('_components.boxSelectInput')
                 @slot('for', 'type')
                 @slot('content', 'Tipo de patrocinio: ')
-                @slot('name', 'type')
                 @slot('id', 'type')
                 @slot('more_options')
                     <option value="1">Por jugador</option>
@@ -57,7 +56,7 @@
             <div class="flex">
                 <input type="submit" value="Crear"
                     class="m-2 w-full mt-4 bg-purple-500 text-white py-2 px-4 rounded hover:bg-purple-600 transition cursor-pointer" />
-                <a href="{{ route('sponsors.index') }}"
+                <a href="{{ route('user.index') }}"
                     class="m-2 text-center w-full mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition cursor-pointer">Cancelar</a>
             </div>
         </form>
