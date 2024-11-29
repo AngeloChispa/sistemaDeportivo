b389e0ee6d9ce279ceb5b74e5cba8fe78c472ea6
JulissaGro <2230163@upv.edu.mx>
2024-11-28 23:05:05 -0600

    show de equipo, ahora está más bonito

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -1,50 +1,106 @@
 @extends('layouts.admin_view')
 
-@section('title', 'Admin')
+@section('title', 'Los jaguares de la UPV')
 
 @section('content')
     <div class="flex flex-1">
         <div class="container flex flex-col lg:flex-row w-full px-6 py-6 gap-6">
             <div class="profile-card bg-stone-800 rounded-lg shadow-md p-6 text-center w-full lg:w-1/3">
-                <img src="{{asset('assets/img/usuario_icon_default.png')}}" alt="Foto de Juan Pérez" class="w-30 h-30 rounded-full mx-auto mb-4">
-                <p class="nombreUsuario text-xl text-red-500 font-bold uppercase mb-2">Juan Pérez</p>
-                <p class="text-sm text-stone-400 mb-4">juanPerez777@hotmail.com</p>
-                <div class="profile-info">
-                    <h5 class="text-lg text-red-500 font-semibold mb-2">Presentación</h5>
-                    <p class="text-sm">Hola Mundo, es hora de patear.</p>
-                </div>
-                <button class="w-full bg-red-600 hover:bg-red-700 text-white font-medium py-2 rounded-md mt-4">Editar
-                    Perfil</button>
+                <img src="{{ asset('assets/img/usuario_icon_default.png') }}" alt="Escudo Los jaguares de la UPV"
+                    class="w-30 h-30 rounded-full mx-auto mb-4">
+                <p class="name text-xl text-red-500 font-bold uppercase mb-2">Los jaguares de la UPV</p>
+                <p class="text-sm">Tamaulipas, cd.Victoria</p>
             </div>
 
             <div class="info-container flex-1 flex flex-col gap-6">
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
-                    <h5 class="text-lg text-red-500 font-semibold mb-4">Información Personal</h5>
+                    <h5 class="text-lg text-red-500 font-semibold mb-4">Información general del equipo</h5>
                     <ul class="text-sm space-y-2">
-                        <li><strong>Nombre:</strong> Juan Pérez</li>
-                        <li><strong>Email:</strong> juanPerez777@hotmail.com</li>
-                        <li><strong>Edad:</strong> 25 años</li>
-                        <li><strong>Deporte:</strong> Fútbol</li>
-                        <li><strong>Rol:</strong> Jugador</li>
+                        <li><strong>Nombre: </strong>Los jaguares de la UPV</li>
+                        <li><strong>Estado: </strong>Tamaulipas</li>
+                        <li><strong>Ciudad: </strong>cd.Victoria</li>
+                        <hr class="h-px my-8 bg-stone-500 border-0">
+                        <li><strong>Deporte: </strong>Natación</li>
+                        <li><strong>Jugadores en plantilla: </strong>10</li>
                     </ul>
                 </div>
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
-                    <h5 class="text-lg text-red-500 font-semibold mb-4">Estadísticas</h5>
-                    <ul class="text-sm space-y-2">
-                        <li><strong>Partidos Jugados:</strong> 30</li>
-                        <li><strong>Goles Marcados:</strong> 5</li>
-                        <li><strong>Asistencias:</strong> 3</li>
-                    </ul>
-                </div>
-                <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
-                    <h5 class="text-lg text-red-500 font-semibold mb-4">Logros</h5>
-                    <ul class="text-sm space-y-2">
-                        <li>Ganador del Balón de Oro 2024</li>
-                        <li>Campeón de la Liga MX 2024</li>
-                        <li>Ganador del Premio Puskás 2024</li>
-                    </ul>
+                    <h5 class="text-lg text-red-500 font-semibold mb-4">Títulos</h5>
+                    <div class="flex flex-row">
+                        <td>
+                            <tr><img class="w-32 h-32 mx-2" src="{{asset('assets/img/usuario_icon_default.png')}}" alt="titulo"></tr>
+                            <tr><img class="w-32 h-32 mx-2" src="{{asset('assets/img/usuario_icon_default.png')}}" alt="titulo"></tr>
+                            <tr><img class="w-32 h-32 mx-2" src="{{asset('assets/img/usuario_icon_default.png')}}" alt="titulo"></tr>
+                            <tr><img class="w-32 h-32 mx-2" src="{{asset('assets/img/usuario_icon_default.png')}}" alt="titulo"></tr>
+                        </td>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="px-6 gap-6">
+        @component('_components.table_export')
+            @slot('title')
+                Jugadores
+            @endslot
+            @slot('p_content')
+                Jugadores del equipo.
+            @endslot
+            @slot('content_head')
+                <tr>
+                    {{-- @empty($variable)
+                            <th>No hay jugadores asignados</th>
+                        @else
+                            <th>Nombre</th>
+                            <th>Apellidos</th>
+                            <th>Fecha de Nacimiento</th>
+                            <th>Nacionalidad</th>
+                            <th>Estado</th>
+                            <th>Altura</th>
+                            <th>Fecha de registro</th>
+                        @endempty --}}
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                    <th>Fecha de Nacimiento</th>
+                    <th>Lugar de nacimiento</th>
+                    <th>Estado</th>
+                </tr>
+            @endslot
+            @slot('content_body')
+                {{-- @forelse ($people as $person)
+                        @if ($person->player)
+                            <tr class="border-b border-stone-700 h-16">
+                                <td>{{ $person->name }}</td>
+                                <td>{{ $person->lastname }}</td>
+                                <td>{{ $person->birthdate }}</td>
+                                <td>{{ $person->nationality }}</td>
+                                <td>{{ $person->nationality }}</td>
+                            </tr>
+                        @endif
+                    @empty
+                    @endforelse --}}
+                <tr class="border-b border-stone-700 h-16">
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                </tr>
+                <tr class="border-b border-stone-700 h-16">
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                </tr>
+                <tr class="border-b border-stone-700 h-16">
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                    <th>a</th>
+                </tr>
+            @endslot
+        @endcomponent
     </div>
 @endsection
