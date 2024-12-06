4adf23abff50844e41111e0eecf40468367db8ca
Jesus Nuñez <2230224@upv.edu.mx>
2024-12-06 13:17:31 +0000

    VIstas show

--- resources/views/players/show.blade.php
+++ resources/views/players/show.blade.php
@@ -7,8 +7,8 @@
         <div class="container flex flex-col lg:flex-row w-full px-6 py-6 gap-6">
             <div class="profile-card bg-stone-800 rounded-lg shadow-md p-6 text-center w-full lg:w-1/3">
                 <img src="{{asset('assets/img/usuario_icon_default.png')}}" alt="Foto de Juan Pérez" class="w-30 h-30 rounded-full mx-auto mb-4">
-                <p class="nombreUsuario text-xl text-red-500 font-bold uppercase mb-2">Juan Pérez</p>
-                <p class="text-sm text-stone-400 mb-4">Retirado</p>
+                <p class="nombreUsuario text-xl text-red-500 font-bold uppercase mb-2">{{$person->name}} {{$person->lastname}}</p>
+                <p class="text-sm text-stone-400 mb-4">{{$person->player->status}}</p>
                 <div class="pt-4">
                     <h5 class="text-lg text-red-500 font-semibold mb-4">Estadísticas</h5>
                     <ul class="text-sm space-y-2">
@@ -23,13 +23,13 @@
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
                     <h5 class="text-lg text-red-500 font-semibold mb-4">Información Personal</h5>
                     <ul class="text-sm space-y-2">
-                        <li><strong>Id: </strong>idUsuario</li>
-                        <li><strong>Nombre: </strong>Nombre completo con apellidos</li>
-                        <li><strong>Fecha de nacimiento: </strong>10/10/2010</li>
-                        <li><strong>Lugar de nacimiento: </strong>usuario lugar nacimiento</li>
-                        <li><strong>Nacionalidad: </strong>usurio nacionalidad</li>
-                        <li><strong>Altura: </strong>1.74</li>
-                        <li><strong>Lado dominante: </strong>Izquierdo</li>
+                        <li><strong>Id: </strong>{{$person->id}}</li>
+                        <li><strong>Nombre: </strong>{{$person->name}} {{$person->lastname}}</li>
+                        <li><strong>Fecha de nacimiento: </strong>{{$person->birthdate}}</li>
+                        <li><strong>Lugar de nacimiento: </strong>{{$person->birthplace}}</li>
+                        <li><strong>Nacionalidad: </strong>{{$person->birthplace}}</li>
+                        <li><strong>Altura: </strong>{{$person->player->height}}</li>
+                        <li><strong>Lado dominante: </strong>{{$person->player->bestSide}}</li>
                     </ul>
                 </div>
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,28 +9,28 @@
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
                     <h5 class="text-lg text-red-500 font-semibold mb-4">Información Personal</h5>
                     <ul class="text-sm space-y-2">
-                        <li><strong>Nombre: </strong>Nombre completo con apellidos</li>
-                        <li><strong>Fecha de nacimiento: </strong>10/10/2010</li>
-                        <li><strong>Lugar de nacimiento: </strong>usuario lugar nacimiento</li>
-                        <li><strong>Nacionalidad: </strong>usurio nacionalidad</li>
-                        <li><strong>Teléfono: </strong>834776112</li>
+                        <li><strong>Nombre: </strong>{{$person->name}}</li>
+                        <li><strong>Fecha de nacimiento: </strong>{{$person->birthdate}}</li>
+                        <li><strong>Lugar de nacimiento: </strong>{{$person->birthplace}}</li>
+                        <li><strong>Nacionalidad: </strong>{{$person->birthplace}}</li>
+                        <li><strong>Teléfono: </strong>{{$person->user->phone}}</li>
                     </ul>
                 </div>
                 <div class="stats bg-stone-800 rounded-lg shadow-md p-6">
                     <h5 class="text-lg text-red-500 font-semibold mb-4">Información general</h5>
                     <ul class="text-sm space-y-2">
-                        <li><strong>Id: </strong>idUsuario</li>
-                        <li><strong>Username: </strong>nomreUsuario</li>
-                        <li><strong>Correo: </strong>correo@usuario.ejemplo.me</li>
-                        <li><strong>Fecha de registro:</strong>fecha de registro</li>
+                        <li><strong>Id: </strong>{{$person->user->id}}</li>
+                        <li><strong>Username: </strong>{{$person->user->username}}</li>
+                        <li><strong>Correo: </strong>{{$person->user->email}}</li>
+                        <li><strong>Fecha de registro:</strong>{{$person->user->up_date}}</li>
                     </ul>
                 </div>
             </div>
             <div class="profile-card bg-stone-800 rounded-lg shadow-md p-6 text-center w-full lg:w-1/3">
                 <img src="{{ asset('assets/img/usuario_icon_default.png') }}" alt="Foto de Juan Pérez"
                     class="w-30 h-30 rounded-full mx-auto mb-2">
-                <p class="nombreUsuario text-xl text-red-500 font-bold uppercase">Username</p>
-                <button class="w-full bg-red-600 hover:bg-red-700 text-white font-medium py-2 rounded-md mt-4">Editar
+                <p class="nombreUsuario text-xl text-red-500 font-bold uppercase">{{$person->user->username}}</p>
+                <button action="{{route('user.edit', $person->id)}}" class="w-full bg-red-600 hover:bg-red-700 text-white font-medium py-2 rounded-md mt-4">Editar
                     Perfil</button>
             </div>
 
