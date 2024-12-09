b75179b4a157181d4fe0bfc628e01fa673fc405c
Jesus Nuñez <2230224@upv.edu.mx>
2024-12-09 01:42:03 +0000

    Apañalo Julissa

--- resources/views/layouts/_partials/navbar.blade.php
+++ resources/views/layouts/_partials/navbar.blade.php
@@ -5,8 +5,8 @@
             <div class="flex items-center justify-start rtl:justify-end">
                 <a href="{{ route('index') }}" class="flex ms-2 md:me-24">
                     <img src={{ asset('assets/img/logotipo.png') }} class="h-10 me-3" alt="Logo" />
-                    <span
-                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Jogo Bonito</span>
+                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Jogo
+                        Bonito</span>
                 </a>
             </div>
 
@@ -14,8 +14,7 @@
             <div
                 class="relative flex items-center bg-stone-800 p-1 px-4 rounded-full duration-200 w-[300px] rounded-lg border border-zinc-700 focus-within:ring focus-within:ring-red-500 hover:bg-stone-700 hover:border-zinc-500">
 
-                <ion-icon name="search-outline"
-                    class="text-zinc-400 text-xl mr-2 duration-200 hover:text-zinc-200">
+                <ion-icon name="search-outline" class="text-zinc-400 text-xl mr-2 duration-200 hover:text-zinc-200">
                 </ion-icon>
 
                 {{-- TODO: Cambiar esto por una petición POST o como gusten --}}
@@ -25,17 +24,20 @@
                         placeholder="Buscar...">
                 </form>
             </div>
-
             {{-- Usuario --}}
             <div class="flex items-center">
                 <div class="flex items-center ms-3">
-                    <div class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                        aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                        <span class="sr-only">Open user menu</span>
-                        <img class="w-10 h-10 rounded-full" src="{{ asset('assets/img/usuario_icon_default.png') }}"
-                            alt="user photo">
-
-                    </div>
+                    @auth
+                        <div class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                            <span class="sr-only">Open user menu</span>
+                            <img class="w-10 h-10 rounded-full" src="{{ asset('assets/img/usuario_icon_default.png') }}"
+                                alt="user photo">
+                        </div>
+                    @else
+                        <a href="{{ route('login') }}">Log in</a>
+                        <a href="{{ route('register') }}">Registrarse</a>
+                    @endauth
                 </div>
             </div>
         </div>
